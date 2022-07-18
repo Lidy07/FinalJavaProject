11a1f0ebcf98986b1deb7ce958b4a003bf96b966
Lidy07 <lidiyageneration@gmail.com>
2022-07-18 10:02:44 -0400

    some file added with some specific changes

---
+++ test/com/generation/service/StudentServiceTest.java
@@ -0,0 +1,29 @@
+package com.generation.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void subscribeStudent() {
+    }
+
+    @Test
+    void findStudent() {
+    }
+
+    @Test
+    void showSummary() {
+    }
+
+    @Test
+    void enrollToCourse() {
+    }
+}
